a73c721021ac3e2fadc9fad03807997ecf01a601
Caleb <kamenjuhkiiru@gmail.com>
2023-12-04 06:46:01 +0300

    Completed assignment and revision

--- lib/book.py
+++ lib/book.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 
 class Book:
+    
     def __init__(self, title, page_count):
         self.title = title
-        self._page_count = page_count
-
+        self.page_count = page_count
+        
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
-    
+        
     def get_page_count(self):
-        print(self._page_count)
         return self._page_count
     
-    def set_page_count(self, new_page_count):
-        if (type(new_page_count) == int):
-            self._page_count = new_page_count
-            print("page_count set to", new_page_count)
+    def set_page_count(self, page_count):
+        if (type(page_count) == int):
+            self._page_count = page_count
         else:
             print("page_count must be an integer")
     
